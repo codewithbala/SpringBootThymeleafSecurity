6db45569d2addd845c8bbdb768f5767d3ac0277a
G S Gowtham <G S Gowtham@Gowtham>
2023-03-14 18:28:49 +0530

    sysout added

---
+++ springthymeleaf/springthymeleaf/src/main/java/com/student/springthymeleaf/SpringthymeleafApplication.java
@@ -0,0 +1,15 @@
+package com.student.springthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringthymeleafApplication {
+
+	public static void main(String[] args) {
+		
+		System.out.println("application started");
+		SpringApplication.run(SpringthymeleafApplication.class, args);
+	}
+
+}
